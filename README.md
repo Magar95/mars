b5b67bf2143061b6af99766dea0f9538bbc11377
Muhammad Akhmadov <magaakhmadov@mail.ru>
2023-12-29 15:53:23 +0300

    [upd] админка, модель заявки

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MARS PROJECT
+
+Суперюзер:
+
+Юзернейм: magar95
+email: magaakhmadov@mail.ru
+psw: 01alohan
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from  django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy
+from django.db import models
+from datetime import datetime
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Device(models.Model):
+    """Оборудование"""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    class Meta:
+        dv_table = 'devices'
+        verbose_name = 'Доступное оборудование'
+        verbose_name_plural = 'Доступное оборудование'
+
+    manufacturer = models.TextField(verbose_name='Производитель')
+    model = models.TextField(verbose_name='Модель')
+
+    def __str__(self):
+        return f'{self.manufacturer} {self.model}'
+
+
+class Customer(models.Model):
+    """Конечные пользователи оборудования"""
+
+    class Meta:
+        dv_table = 'customers'
+        verbose_name = 'Описание контрагента'
+        verbose_name_plural = 'Описание контрагента'
+
